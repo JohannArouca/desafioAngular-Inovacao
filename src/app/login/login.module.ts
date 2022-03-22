91050597c10b1212adc3650aea585d803b7055e7
JohannArouca <aroucajohann@yahoo.com>
2022-03-22 17:59:26 -0300

    alteracoes na interface

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,6 +15,7 @@ import { LoginComponent } from './login.component';
     CommonModule,
     MatCardModule,
     MatFormFieldModule,
+    MatInputModule,
     MatButtonModule,
     FormsModule,
     ReactiveFormsModule,
